b59d15e39fa50e5fde2e5ffb6c63ac5d4a3c356c
fmsarasilva <38432794+fmsarasilva@users.noreply.github.com>
2018-07-09 23:46:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -48,8 +48,8 @@ $(function () {
             self.diff = ((self.loaded / 100) * Math.PI * 2 * 10).toFixed(2);
             self.context.clearRect(0, 0, self.width, self.height);
             self.context.lineWidth = 10;
-            self.context.fillStyle = "#000";
-            self.context.strokeStyle = "#f4ead9";
+            self.context.fillStyle = "#dfa88b";
+            self.context.strokeStyle = "#dfa88b";
             self.context.textAlign = "center";
 
             self.context.fillText(self.loaded + "%", self.width * .5, self.height * .5 + 2, self.width);
@@ -165,3 +165,4 @@ $(document).ready(function ($) {
     });
 
 }(jQuery));
+
